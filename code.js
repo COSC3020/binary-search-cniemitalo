4339ca9a169ab5afd7a3462655262e080de1ef8d
cniemitalo <159483630+cniemitalo@users.noreply.github.com>
2025-02-10 11:07:37 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,21 @@
 function binarySearch(list, element) {
-    for (var i = 0; i < list.length; i++) {
-        if (element === list[i]) {
-            return i; 
-        }
-    } 
+    //set high and low to encompass entire array 
+    var low = 0; 
+    var high = list.length - 1; 
+
+    //loop through while there is one or more elements to check 
+    while (high >= low) {
+        var mid = low + Math.floor((high - low) / 2); 
+        //return index when element is found 
+        if (list[mid] == element) {
+            return mid; 
+        } 
+        //search the left or right half of the array 
+        if (list[mid] < element) {
+            low = mid + 1; 
+        } else {
+            high = mid - 1; 
+        } 
+    //return -1 if element is not found 
     return -1;
 }
